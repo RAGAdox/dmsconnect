714aeb0c1b5397664f4139ab349df975aeeca504
Rishi Mukherjee <rishitech882@gmail.com>
2025-03-28 16:31:34 +0530

    fix(onboard-api): fix allow only POST api calls

--- src/pages/api/onboard.ts
+++ src/pages/api/onboard.ts
@@ -10,6 +10,10 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method not allowed" });
+  }
+
   const { course, registrationNumber, endYear, startYear } =
     req.body as OnboardingArgs;
 
