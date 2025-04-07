eeef8bfebd66fd90c010c66e6ca6eb963921aa9a
Rishi Mukherjee <rishitech882@gmail.com>
2025-04-07 05:36:45 +0530

    chore(CICD): Setup CICD Pipeline for auto migration --- Checkpoint

--- bin/vercel/sync-env.sh
+++ bin/vercel/sync-env.sh
@@ -3,5 +3,5 @@
 doppler secrets download --no-file --format json | \
   jq -r 'to_entries[] | "\(.key)=\(.value)"' | \
   while IFS='=' read -r key value; do
-    vercel env add "$key" preview <<< "$value"
+    vercel env add "$key" preview --token="$VERCEL_TOKEN" <<< "$value"
   done
