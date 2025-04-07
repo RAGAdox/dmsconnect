cb71415d80cd01911101a0e011befdbabee6b129
Rishi Mukherjee <rishitech882@gmail.com>
2025-04-07 18:39:53 +0530

    fix(env_vars): fix env push mechanism to vercel

--- bin/vercel/sync-env.sh
+++ bin/vercel/sync-env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+#Check if we can directly download a .env from doppler
 doppler secrets download --no-file --format json | \
   jq -r 'to_entries[] | "\(.key)=\(.value)"' | \
-  while IFS='=' read -r key value; do
-    trimmed_value=$(echo "$value" | xargs)
-    vercel env add "$key" preview --token="$VERCEL_TOKEN" <<< "$trimmed_value"
+  while IFS='=' read -r key value; do 
+    echo "$value" | vercel env add "$key" preview --token="$VERCEL_TOKEN"
   done
